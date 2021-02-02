e9015e4a8f8cf41e1c253173c67108d6acb970d6
Richardson Clayton <70027084+Richardson-clayton@users.noreply.github.com>
2021-02-01 22:02:31 -0300

    Desafio-Java
    Estudo da linguegem java.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Desafio01-java
- Soluções de problemas básicos em java
+### Soluções de problemas básicos em java
+
+01- Crie um programa que leia um número e mostre os números pares até esse numero, inclusive ele mesmo.
+
---
+++ desafio01.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main{
+    public static void main( String args[]){
+        int poi, num, contador = 0;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Informe um número: ");
+        num = in.nextInt();
+        poi = (num%2);
+        if (poi == 0){
+            while (contador < num){
+                contador ++;
+                if(contador%2==0)
+                /*int [] a;
+                a = new int[contador]; */
+                System.out.println(contador);
+            }
+        }
+    }
+}
